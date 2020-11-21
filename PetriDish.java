4ffd7fe931efcfd1ad1bfefb6d90e23a4edf1576
Steven Khaw <72418611+stevenkhaw@users.noreply.github.com>
2020-11-20 16:02:27 -0800

    Update PetriDish.java

--- PetriDish.java
+++ PetriDish.java
@@ -1,3 +1,17 @@
 public class PetriDish {
     
+    //INSTANCE VARIABLE
+    public Cell[][] dish;
+
+    public PetriDish(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; i++) {
+                
+            }
+        }
+    }
+    
+    public String toString() {
+
+    }
 }
